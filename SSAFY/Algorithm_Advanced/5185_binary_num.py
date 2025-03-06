b57dc00ec9fffc8ce47622743054f5d202081531
임유나 <libbyyouna@gmail.com>
2025-03-06 14:09:36 +0900

    D2

---
+++ SSAFY/Algorithm_Advanced/5185_binary_num.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('5185_input.txt')
+t = int(input())
+for tc in range(1, t+1):
+    hex_len, hex_str  = map(str, input().split())
+    hex_len = int(hex_len)
+    dec_num = 0
+    bin_str = ''
+
+    for i in range(hex_len):
+        dec_num = int(hex_str[i], 16) 
+        bin_str += str(format(dec_num, '04b'))
+        
+
+    print(f'#{tc} {bin_str}')
