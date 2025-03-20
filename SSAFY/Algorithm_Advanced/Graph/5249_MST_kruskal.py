1d1ad6611edd27f641a2a8f7f60f8d46d31434bf
임유나 <libbyyouna@gmail.com>
2025-03-20 18:53:51 +0900

    D4 kruskal_mst

--- SSAFY/Algorithm_Advanced/Graph/5249_MST_kruskal.py
+++ SSAFY/Algorithm_Advanced/Graph/5249_MST_kruskal.py
@@ -3,13 +3,24 @@ sys.stdin = open('./input/5249.txt')
 
 def kruskal_mst(edges_list, V):
     
-    parent = [i for i in range(V)]
+    parent = [i for i in range(V+1)]
     rank = [0] * (V+1)
+
+    mst_cost = 0
+
+    for w, s, e in edges_list:
+        if find_set(parent, s) != find_set(parent, e):
+            union(parent, rank, s, e)
+            mst_cost += w
+
+    return mst_cost
+    
 
 def find_set(parent, x):
     if parent[x] != x:
-        parent[x] = find_set(parent[x])
+        parent[x] = find_set(parent, parent[x])
     return parent[x]
+
 
 def union(parent, rank, x , y):
     root_x, root_y = find_set(parent, x), find_set(parent, y)
@@ -31,18 +42,16 @@ t = int(input())
 for tc in range(1, t+1):
     V, E = map(int, input().split())
 
-    edges_list = [[]  for _ in range(V+1)]
+    edges_list = []
 
     # 인접 리스트 생성
     for _ in range(E):
         s, e, w = map(int, input().split())
-        edges_list[s].append((e, w))
-        edges_list[e].append((s, w))
+        edges_list.append((w, s, e))
 
+    # print(edges_list)
+    edges_list.sort(key= lambda x: x[0])
     # 가중치가 낮은 순서대로 인접 리스트 정렬
-    for i in edges_list:
-        i.sort(key= lambda x: x[1])
-    print(edges_list)
 
     result = kruskal_mst(edges_list, V)
 
