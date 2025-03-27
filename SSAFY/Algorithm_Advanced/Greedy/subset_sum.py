93f4404b02d4554bf257587077cd18379e1182ea
임유나 <libbyyouna@gmail.com>
2025-03-27 17:13:01 +0900

    기본 개념 잘 숙지하기

---
+++ SSAFY/Algorithm_Advanced/Greedy/subset_sum.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open('./input/subset_sum.txt')
+
+def recur(cnt, start):
+    if cnt == len(arr):
+        path = []
+        for i in range(len(arr)):
+            if used[i] == True:
+                path.append(arr[i])
+        if sum(path) == 0:
+            print(path)
+        return
+
+    for i in range(start, len(arr)):
+        used[i] = True
+        recur(cnt+1, i+1)
+
+        used[i] = False
+        recur(cnt+1, i+1)
+
+
+
+arr = list(map(int, input().split()))
+
+used = [False] * len(arr)
+
+recur(0, 0)
